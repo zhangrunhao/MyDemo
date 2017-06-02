39fed0381e3f41e18a0f4e8db1ddae4c1f6a4c8f
zhangrh <zhangrhweb@163.com>
2017-06-02 15:15:04 +0800

    完成发送请求, 渲染列表

--- LotTime/src/page/index.vue
+++ LotTime/src/page/index.vue
@@ -6,21 +6,26 @@
     </ul>
   </div>
 </template>
+
 <script>
 export default {
   data () {
     return {
-      lists:
-      [
-        {
-          id: 1,
-          title: 'test title 1'
-        },
-        {
-          id: 2,
-          title: 'test title 2'
-        }
-      ]
+      lists: []
+    }
+  },
+  created () {
+    // 组件创建完成后获取数据, 这里和1.0不一样, 改成了这个样子
+    this.get_data()
+  },
+  methods: {
+    get_data: function (params) {
+      var v = this
+      if (!params) params = {}
+      // 我们这里使用全局绑定的 $api方法来获取数据
+      v.$api.get('topics', params, function (r) {
+        v.lists = r.data
+      })
     }
   }
 }
