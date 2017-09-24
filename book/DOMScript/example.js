7a8c63de448918f574622deaaa45eeb501572548
zhangrh <zhangrhweb@163.com>
2017-09-25 06:58:38 +0800

    添加了一个book文件夹用来写看书过程中的案例, 第一本, JavaScriptDOM编程艺术

---
+++ book/DOMScript/example.js
@@ -0,0 +1,80 @@
+// var temp_fahrenheit = 95
+// var temp_celsius = (temp_fahrenheit - 32) / 1.8
+
+// var message = "I am felling " + "happy"
+// var mood = "happy"
+// var message = "I am felling " + mood
+// var year = 2000
+// var message = "This year is "
+// message += year
+// console.log(message)
+
+// var a = 'ss'
+// var b = 1
+// if (a == b) {
+//   console.log('a=b')
+// }
+
+console.log(window)
+
+
+
+
+// var total = (1 + 4) * 5
+// console.log(total)
+
+// var lennon = Object()
+// lennon.name = 'aaa'
+// lennon.age = 13
+// lennon.living = false
+// var lennon = {
+//   name: 'aaa',
+//   year: 13,
+//   living: false
+// }
+// var beatles = Array()
+// beatles[0] = lennon
+// beatles.vocalist = lennon
+
+// console.log(beatles)
+
+
+
+// var beatles = Array(4)
+// beatles[0] = 'John'
+// beatles[1] = 'Paul'
+// beatles[2] = 'George'
+// beatles[3] = 'Ringo'
+// var beatles = Array("aa", "bb", "cc", "dd")
+// var name = "John"
+// var beatles = ["aa", "bb", 11, 22, name]
+// var names = ['zhang', 'li']
+// beatles[1] = names[0]
+
+// var lennon = ["John", 1940, false]
+// var beatles = []
+// beatles['name'] = lennon
+// var lennon = Array()
+// lennon['name'] = 'John'
+// lennon['year'] = '1940'
+// lennon['living'] = false
+// console.log(lennon)
+
+
+// mood = 'happy'
+// age = 33
+// var mood = 'happy'
+// var age = 33
+// var mood, age
+// var mood = "about 5'10\\ tall"
+// var age = 33.25
+// var temperature = -20.3333333333333
+// alert(mood)
+// alert(age)
+// alert(temperature)
+
+// 这是单行注释
+/* 
+这是一个多行注释
+多行的
+*/
