6ec960dba03c739eae7373dd21d388e4e29b17d8
zhangrh <zhangrhweb@163.com>
2017-10-19 14:28:44 +0800

    完成官方例子 - md编辑器

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Remarkable from 'remarkable'
 
-import './index.css';
+import registerServerWorker from './registerServiceWorker'
 
-import App from './App';
+class MarkdownEditor extends React.Component {
+  constructor(props) {
+    super(props)
+    // 好像所有的方法都要经过这个处理, 才能调用到. 我记得也不是啊..
+    this.handleChange = this
+      .handleChange
+      .bind(this)
+    this.state = {
+      value: 'Type some *markdown* here!'
+    }
+  }
 
-import registerServiceWorker from './registerServiceWorker';
+  // 输入框内容改变
+  handleChange(event) {
+    // 改变文本内容
+    this.setState({value: event.target.value})
+  }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  // 内容经过md语法处理, 传入字符串, 返回html文本
+  getRawMarkup() {
+    // 处理字符串, 用md语法进行处理, 变为html格式
+    const md = new Remarkable()
+    return {
+      __html: md.render(this.state.value)
+    }
+  }
 
-registerServiceWorker();
+  render() {
+    return (
+      <div>
+        <h3>输入内容</h3>
+        <textarea onChange={this.handleChange} defaultValue={this.state.value}/>
+        <h3>输出内容</h3>
+        <div className="content" dangerouslySetInnerHTML={this.getRawMarkup()}></div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <MarkdownEditor/>, document.getElementById('root'))
+
+registerServerWorker()
