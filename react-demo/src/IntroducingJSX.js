6af0dd4f6fc9d95f15e593c79b3ef11998c338c5
zhangrh <zhangrhweb@163.com>
2017-10-20 10:09:37 +0800

    React官网 第一部分 第三章 Introducing JSX

--- react-demo/src/IntroducingJSX.js
+++ react-demo/src/IntroducingJSX.js
@@ -1,14 +1,16 @@
 
 import React, {Component} from 'react'
 
-function formatName(user) {
-  return user.firestName + ' ' + user.lastName
-}
+import './test.css'
+// function formatName(user) {
+//   return user.firestName + ' ' + user.lastName
+// }
 
-const user = {
-  firestName: 'zhang',
-  lastName: 'rh'
-}
+// const user = {
+//   firestName: 'zhang',
+//   lastName: 'rh',
+//   avatarUrl: 'https://ss3.baidu.com/-rVXeDTa2gU2pMbgoY3K/it/u=1759972132,4039059816&fm=202&src=762&mola=new&crop=v1'
+// }
 
 // const element = (
 //   <h1>
@@ -16,17 +18,30 @@ const user = {
 //   </h1>
 // )
 
-function getGreeting(user) {
-  if (user) {
-    return <h1>Hello, {formatName(user)} !</h1>
-  }
-  return <h1>Hello, kkkkkkk</h1>
-}
+// function getGreeting(user) {
+//   if (user) {
+//     return <h1>Hello, {formatName(user)} !</h1>
+//   }
+//   return <h1>Hello, kkkkkkk</h1>
+// }
+
+// const element = (
+//   <div
+//     tabIndex="0"
+//   >
+//   </div>
+// )
+ 
+const element = (
+  <h1 className="greeting">
+    Hello world
+  </h1>
+)
 
 
 class IntroducingJSX extends Component {
   render() {
-    return (getGreeting())
+    return (element)
   }
 }
 
