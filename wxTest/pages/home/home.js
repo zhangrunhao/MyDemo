688702b63d96e57bf7aaa787be03b67a5a667031
zhangrh <zhangrhweb@163.com>
2017-10-22 18:19:09 +0800

    完成home页面的大体展示

--- wxTest/pages/home/home.js
+++ wxTest/pages/home/home.js
@@ -2,8 +2,17 @@ const app = getApp()
 
 Page({
   data: {
-    iconUrl: {
-      src: 'http://oy3ywgz62.bkt.clouddn.com/jige/open-eye_03.png'
-    }
+    iconUrl: 'http://oy3ywgz62.bkt.clouddn.com/jige/open-eye_03.png',
+    everyDayImages: [
+      'http://img1.imgtn.bdimg.com/it/u=735777041,1005463406&fm=27&gp=0.jpg',
+      'http://img3.imgtn.bdimg.com/it/u=1161269879,1499625874&fm=27&gp=0.jpg',
+      'http://img0.imgtn.bdimg.com/it/u=2949757311,587718453&fm=27&gp=0.jpg',
+      'http://img4.imgtn.bdimg.com/it/u=1922157828,824239438&fm=27&gp=0.jpg'
+    ]
+  },
+  goChatPage() {
+    wx.navigateTo({
+      url: '../chat/chat'
+    })
   }
 })
