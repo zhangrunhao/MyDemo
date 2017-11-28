fa51feba72c3f7abd882b6caa0f84e059fa8c2e5
zhangrh <zhangrhweb@163.com>
2017-11-28 16:42:48 +0800

    webpack doc study develop entirment

--- webpack-doc/webpack.config.js
+++ webpack-doc/webpack.config.js
@@ -7,6 +7,10 @@ module.exports = {
         app: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
@@ -15,6 +19,7 @@ module.exports = {
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
     }
 }
