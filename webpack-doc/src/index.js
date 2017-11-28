eea8997798f188cef556c082e875418c8d7eda03
zhangrh <zhangrhweb@163.com>
2017-11-28 15:52:31 +0800

    webpack doc study Asset Management

--- webpack-doc/src/index.js
+++ webpack-doc/src/index.js
@@ -1,8 +1,24 @@
 import _ from 'lodash'
+import './style.css'
+import Icon from './icon.png'
+import Data from './data.xml'
 
 function component() {
     var element = document.createElement('div')
+
+    // 引入Loadsh, 现在由此脚本引入
     element.innerHTML = _.join(['Hello', 'Webpack', ''])
+    element.classList.add('hello')
+
+    // 将图像添加到我们现有的div
+    var myIcon = new Image()
+    myIcon.src = Icon
+
+
+    element.appendChild(myIcon)
+
+    console.log(Data)
+
     return element
 }
 
--- webpack-doc/webpack.config.js
+++ webpack-doc/webpack.config.js
@@ -5,5 +5,34 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|swg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
     }
 }
