8d2dbf7b0799609b9b498c2f0d5db82ef7251ddc
zhangrh <zhangrhweb@163.com>
2017-11-28 20:50:41 +0800

    webpack doc study production

--- webpack-doc/src/index.js
+++ webpack-doc/src/index.js
@@ -1,30 +1,16 @@
-import _ from 'lodash'
-import printMe from './print.js'
-import './style.css'
+import {cube} from './math.js'
 
 function component() {
-    var element = document.createElement('div')
+    var element = document.createElement('pre')
     var btn = document.createElement('button')
 
-    element.innerHTML = _.join(['Hello', 'Webpack', ''])
-
-    btn.innerHTML = 'Click me and check the console!'
-    btn.onclick = printMe
-
-    element.appendChild(btn)
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n')
 
     return element
 }
 
-
 let element = component()
 document.body.appendChild(element)
-
-if (module.hot) {
-    module.hot.accept('./print.js', function () {
-        console.log('Accepting the updated printMe module!')
-        document.body.removeChild(element);
-        element = component(); // 重新渲染页面后，component 更新 click 事件处理
-        document.body.appendChild(element);
-    })
-}
--- webpack-doc/webpack.config.js
+++ webpack-doc/webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
     entry: {
@@ -21,6 +22,7 @@ module.exports = {
         ]
     },
     plugins: [
+        new UglifyJSPlugin(),
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output Management'
