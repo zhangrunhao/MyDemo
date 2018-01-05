f3be056629c69be8464f58b7834512eaca9f6f55
zhangrh <zhangrhweb@163.com>
2018-01-05 14:11:56 +0800

    add professional js 4

---
+++ book/ProfessionalJS/04test.js
@@ -0,0 +1,39 @@
+
+var person = {
+  name : 'zhangrh'
+}
+var obj = person
+
+obj = new Object
+
+obj.name = "new"
+
+console.log(person.name)
+
+
+
+// function setName(obj) {
+//   obj.name = "zhangrh"
+//   obj = new Object
+//   obj.name = "lisi"
+// }
+
+// var person = new Object
+// person.name = "222"
+// setName(person)
+// console.log(person.name)
+
+
+// var num = {}
+// num.name = "zhangrh"
+// function add(num) {
+//   num.name = "ddd"
+// }
+
+// add(num)
+// console.log(num)
+
+
+// var name = "zhangrh"
+// name.age = 11
+// console.log(name.age)
