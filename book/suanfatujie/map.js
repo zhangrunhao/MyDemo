12b1202d60787948315a02833eeca220aa4f1ceb
zhangrh <zhangrhweb@163.com>
2018-05-31 10:47:01 +0800

    map youhua

--- book/suanfatujie/map.js
+++ book/suanfatujie/map.js
@@ -1,12 +1,14 @@
+// 即使有双向箭头, 依旧可以找到想要的元素, 及时返回即可, 只是会浪费空间
+// 但在有双向箭头的情况下, 如果没有了正确元素, 就会形成死循环.
 var graph = {
-  you: ['alice', 'bob', 'claire'],
-  bob: ['anuj', 'peggy'],
-  alice: ['peggy'],
-  claire: ['thom', 'jonny'],
+  you: ['bob'],
+  bob: ['anuj', 'alice', 'peggy'],
+  alice: ['peggy', 'bob', 'claire'],
+  claire: ['jonny'],
+  jonny: ['thom'],
   anuj: [],
   peggy: [],
-  thom: [],
-  jonny: []
+  thom: []
 }
 
 var searchArr = []
