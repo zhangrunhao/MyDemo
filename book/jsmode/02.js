01c8c920a3ec190696c40567e42124e2a0303647
zhangrh <zhangrhweb@163.com>
2018-06-04 09:31:50 +0800

    fengzhuang one

--- book/jsmode/02.js
+++ book/jsmode/02.js
@@ -8,18 +8,34 @@
 
 
 
-var caches = {} // 如果计算过的就不再计算了..  
-var mult = function () {
-  var args = Array.prototype.join.call(arguments, ',')
-  if (caches[args]) {
-    return cache[args]
-  }
-  var a = 1
-  for (var i =0, l = arguments.length;i<l;i++) {
-    a = a*arguments[i]
-  }
-  return caches[args] = a
-}
+// var caches = {} // 如果计算过的就不再计算了..  
+// var mult = function () {
+//   var args = Array.prototype.join.call(arguments, ',')
+//   if (caches[args]) {
+//     return cache[args]
+//   }
+//   var a = 1
+//   for (var i = 0, l = arguments.length; i < l; i++) {
+//     a = a * arguments[i]
+//   }
+//   return caches[args] = a
+// }
 
-var res = mult(2,1,3)
+
+var mult = (function () {
+  var caches = {}
+  return function () {
+    var args = Array.prototype.join.call(arguments, ',')
+    if (caches[args]) {
+      return cache[args]
+    }
+    var a = 1
+    for (var i = 0, l = arguments.length; i < l; i++) {
+      a = a * arguments[i]
+    }
+    return caches[args] = a
+  }
+})()
+
+var res = mult(2, 1, 3)
 console.log(res)
