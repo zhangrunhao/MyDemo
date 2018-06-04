63fffbd8c9469d8819eadb4e3a3f8806ad8335f0
zhangrh <zhangrhweb@163.com>
2018-06-04 09:28:30 +0800

    add caches

--- book/jsmode/02.js
+++ book/jsmode/02.js
@@ -1,18 +1,25 @@
+// var mult = function () {
+//   var a = 1
+//   for (var i =0, l = arguments.length;i<l;i++) {
+//     a = a*arguments[i]
+//   }
+//   return a
+// }
+
+
+
+var caches = {} // 如果计算过的就不再计算了..  
 var mult = function () {
+  var args = Array.prototype.join.call(arguments, ',')
+  if (caches[args]) {
+    return cache[args]
+  }
   var a = 1
   for (var i =0, l = arguments.length;i<l;i++) {
     a = a*arguments[i]
   }
-  return a
-};
+  return caches[args] = a
+}
 
 var res = mult(2,1,3)
 console.log(res)
-
-var cache = {}
-var mult = function () {
-  var args = Array.prototype.join.call(arguments, ',')
-  if (caches[args]) {
-    
-  }
-}
